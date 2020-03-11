54ffa74671b9cf0ee6858b6060ddcd0742d7b1c2
Jannis <jannis@pixel-pink.de>
2020-03-12 00:37:31 +0100

    legal pages

---
+++ src/routes/datenschutz.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <meta name="robots" content="noindex" />
+</svelte:head>
+
+<section class="p-x1p5 mb-x2">
+  <h1>Datenschutz</h1>
+</section>
---
+++ src/routes/versicherungsbedingungen.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <meta name="robots" content="noindex" />
+</svelte:head>
+
+<section class="p-x1p5 mb-x2">
+  <h1>Versicherungsbedingungen</h1>
+</section>
