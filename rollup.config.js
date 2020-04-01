3ba5253660e65e9d3e6dcc4d3c54f9512754e2ca
Jannis <jannis@pixel-pink.de>
2020-04-02 00:26:57 +0200

    last try polyfill

--- rollup.config.js
+++ rollup.config.js
@@ -81,21 +81,29 @@ export default {
       !legacy &&
         babel({
           extensions: [".js", ".mjs", ".html", ".svelte"],
-          // runtimeHelpers: true,
+          exclude: ["node_modules/@babel/**"],
+          plugins: [
+            "@babel/plugin-syntax-dynamic-import",
+            "@babel/plugin-proposal-object-rest-spread"
+          ]
+        }),
+
+      legacy &&
+        babel({
+          extensions: [".js", ".mjs", ".html", ".svelte"],
+          runtimeHelpers: true,
           exclude: ["node_modules/@babel/**"],
           presets: [
             [
               "@babel/preset-env",
               {
-                targets: "> 0.25%, not dead",
-                useBuiltIns: "usage",
-                corejs: 3
+                // spec: true,
+                targets: "> 0.25%, ie >= 11, not dead"
               }
             ]
           ],
           plugins: [
             "@babel/plugin-syntax-dynamic-import",
-            "@babel/plugin-proposal-object-rest-spread",
             [
               "@babel/plugin-transform-runtime",
               {
