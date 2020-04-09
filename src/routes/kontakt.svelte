d71e5f3c379e8a8825da6569e3eb76cafe1898ee
Jannis <jannis@pixel-pink.de>
2020-04-09 15:05:30 +0200

    reactivated redirects for all pages

--- src/routes/kontakt.svelte
+++ src/routes/kontakt.svelte
@@ -2,7 +2,7 @@
   import Layout from "../routes/_layouts/layout.svelte";
 </script>
 
-<Layout headline={false}  >
+<Layout>
   <section class="px-x1p5">
     <h1 class="text-primary text-x6 leading-tight border-solid">Kontakt</h1>
   </section>
